39624749ac963f63d32ed6cb273deff06a5e1dfe
Danny Warren <danny@theforbiddenarts.com>
2022-03-23 14:31:48 -0400

    code upload

--- BasicIf/Program.cs
+++ BasicIf/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//bool isComplete = true;
+
+//if (isComplete)
+//{
+//    Console.WriteLine("The statement was True.");
+//    Console.WriteLine("This line works in True.");
+//}
+//else
+//{
+//    Console.WriteLine("The statement was False.");
+//    Console.WriteLine("This should also run.");
+//}
+
+
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+string lastName;
+
+if (firstName.ToLower() == "tim")
+{
+    Console.WriteLine("Hello Mr. Corey.");
+    lastName = "Corey";
+}
+else
+{
+    Console.WriteLine($"Hello {firstName}.");
+    lastName = "Smith";
+}
+
+Console.WriteLine(lastName);
+Console.WriteLine("End of program.");
